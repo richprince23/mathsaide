57abddff7365bbd7868b8df8aa7d3a010e9462c4
Richard Kweku Aikins <richardkns7@gmail.com>
2023-08-26 04:26:12 +0000

    added_view history screen

--- lib/widgets/history_item.dart
+++ lib/widgets/history_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mathsaide/constants/constants.dart';
 import 'package:mathsaide/controllers/session_controller.dart';
+import 'package:mathsaide/screens/home/view_history.dart';
 import 'package:resize/resize.dart';
 
 class HistoryItem extends StatelessWidget {
@@ -16,24 +17,44 @@ class HistoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dateTime = convertDateTimeString(sessionID);
-    return Container(
-      padding: pa1,
-      decoration: ShapeDecoration(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-          side: BorderSide(
-            color: priColDark,
-            width: 2,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return ViewHistoryScreen(
+              sessionID: sessionID,
+              topic: topic,
+            );
+          }),
+        );
+      },
+      child: Container(
+        padding: pa1,
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: BorderSide(
+              color: priColDark,
+              width: 2,
+            ),
+          ),
+          color: bgColDark,
+        ),
+        child: ListTile(
+          title: Text(
+            topic,
+            style: TextStyle(fontSize: 16.sp),
+          ),
+          subtitle: Text(dateTime),
+          trailing: IconButton.outlined(
+            color: Colors.red,
+            onPressed: () {
+              // deleteSession(sessionID);
+            },
+            icon: const Icon(Icons.delete),
           ),
         ),
-        color: bgColDark,
-      ),
-      child: ListTile(
-        title: Text(
-          topic,
-          style: TextStyle(fontSize: 16.sp),
-        ),
-        subtitle: Text(dateTime),
       ),
     );
   }
